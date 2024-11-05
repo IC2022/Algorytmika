9e03289f531f6eab615d6416fcadf62933c61614
IC2022 <99969892+IC2022@users.noreply.github.com>
2024-11-05 22:55:29 +0100

    szybkie_potegowanie.cpp

--- szybkie_potegowanie.cpp
+++ szybkie_potegowanie.cpp
@@ -1,17 +1,17 @@
 #include <iostream>
 using namespace std; 
     int szybkiePotegowanie(int a, int n) {
-    int wynik = 1; // K1: Inicjalizacja wyniku jako 1
+    int wynik = 1; 
  
-    while (n > 0) { // K2: Dopóki n > 0
-        if (n % 2 == 1) { // K3: Jeśli n % 2 = 1
-            wynik *= a; // Mnożymy wynik przez bazę
+    while (n > 0) { 
+        if (n % 2 == 1) { 
+            wynik *= a; 
         }
-        a *= a; // K4: Kwadratowanie bazy
-        n /= 2; // K5: Dzielimy wykładnik przez 2
-    } // K6: Zakończ pętlę
+        a *= a;
+        n /= 2; 
+    } 
  
-    return wynik; // K7: Zwróć wynik
+    return wynik; 
 }
  
 int main() {
