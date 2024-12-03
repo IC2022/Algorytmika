201c7cd76ec9d59ac5d4edf5993b88d568a8c3d2
IC2022 <99969892+IC2022@users.noreply.github.com>
2024-12-03 09:45:15 +0100

    Update potega_rekurencja.cpp

--- potega_rekurencja.cpp
+++ potega_rekurencja.cpp
@@ -1,34 +1,32 @@
 #include <iostream>
 using namespace std;
 
-// Funkcja rekurencyjna obliczająca potęgę liczby
 double CalculatePowerRecursive(double base, int exponent) {
     if (exponent == 0) {
-        return 1; // Każda liczba do potęgi zerowej wynosi 1
+        return 1;
     } else if (exponent > 0) {
         return base * CalculatePowerRecursive(base, exponent - 1);
     } else {
-        return 1 / CalculatePowerRecursive(base, -exponent); // Obsługa ujemnego wykładnika
+        return 1 / CalculatePowerRecursive(base, -exponent);
     }
 }
 
-// Funkcja wyświetlająca wynik
+
 void DisplayResult(double result) {
     cout << "Wynik potęgowania: " << result << endl;
 }
 
-// Funkcja main
+
 int main() {
     double base;
     int exponent;
 
-    // Pobranie danych od użytkownika
+   
     cout << "Podaj liczbę podstawową: ";
     cin >> base;
     cout << "Podaj wykładnik: ";
     cin >> exponent;
 
-    // Obliczenie potęgi i wyświetlenie wyniku
     double result = CalculatePowerRecursive(base, exponent);
     DisplayResult(result);
 
