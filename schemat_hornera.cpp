4246ec818b79c113755f6e4ba390830afd59e250
IC2022 <99969892+IC2022@users.noreply.github.com>
2025-03-25 08:38:51 +0100

    Update schemat_hornera.cpp

--- schemat_hornera.cpp
+++ schemat_hornera.cpp
@@ -1,11 +1,9 @@
 #include <iostream>
 using namespace std;
 
-// Funkcja realizująca schemat Hornera
 double horner(int* wspolczynniki, int stopien, double x) {
-    double wynik = wspolczynniki[0];  // Pierwszy współczynnik (najwyższy stopień)
+    double wynik = wspolczynniki[0]; 
     
-    // Przechodzimy przez pozostałe współczynniki
     for (int i = 1; i <= stopien; ++i) {
         wynik = wynik * x + wspolczynniki[i];
     }
@@ -14,33 +12,24 @@ double horner(int* wspolczynniki, int stopien, double x) {
 }
 
 int main() {
-    int stopien; // Stopień wielomianu
-    double x;    // Argument wielomianu
+    int stopien; 
+    double x;    
     
-    // K2: Deklaracja zmiennych
     cout << "Podaj stopień wielomianu: ";
-    cin >> stopien;  // K3: Pobranie stopnia wielomianu
+    cin >> stopien; 
     
-    // K4: Deklaracja dynamicznej tablicy na współczynniki
     int* wspolczynniki = new int[stopien + 1];
     
-    // K5: Wczytywanie współczynników
     for (int i = 0; i <= stopien; ++i) {
         cout << "Podaj wspolczynnik dla x^" << (stopien - i) << ": ";
-        cin >> wspolczynniki[i];  // K6: Wprowadzenie współczynnika
+        cin >> wspolczynniki[i]; 
     }
     
-    // K7: Pobranie argumentu wielomianu
     cout << "Podaj wartość argumentu (x): ";
-    cin >> x;  // Pobranie wartości x
-    
-    // K8: Wywołanie funkcji Horner
+    cin >> x; 
+
     double wynik = horner(wspolczynniki, stopien, x);
-    
-    // Wyświetlanie wyniku
     cout << "Wynik wielomianu w punkcie x = " << x << " wynosi: " << wynik << endl;
-    
-    // K9: Usunięcie dynamicznej tablicy
     delete[] wspolczynniki;
     
     return 0;
